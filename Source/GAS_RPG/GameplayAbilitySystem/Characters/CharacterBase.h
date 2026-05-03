251ad9c1132ff187a83df40e9302822b22b4a7e0
Brent Hanover <brenthanover@gmail.com>
2026-05-03 16:30:23 -0700

    feat: initial commit

---
+++ Source/GAS_RPG/GameplayAbilitySystem/Characters/CharacterBase.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "CharacterBase.generated.h"
+
+UCLASS()
+class GAS_RPG_API ACharacterBase : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+// variables
+	
+public:
+	// Sets default values for this character's properties
+	ACharacterBase();
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AbilitySystem")
+	class UAbilitySystemComponent* AbilitySystemComponent;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilitySystem")
+	EGameplayEffectReplicationMode AscReplicationMode = EGameplayEffectReplicationMode::Mixed;
+
+// functions
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	virtual void PossessedBy(AController* NewController) override;
+	
+	virtual void OnRep_PlayerState() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+};
